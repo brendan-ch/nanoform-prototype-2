05624370ca0147790ff18c493babae26fdd0d35c
Brendan Chen <me@bchen.dev>
2024-09-27 16:02:49 -0700

    add testing stub for db_methods

---
+++ tests/test_db/test_db_methods.py
@@ -0,0 +1,27 @@
+import unittest
+import sqlite3
+from db import db_methods
+
+# TODO setup in-memory database
+
+class TestDbMethods(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # add_user_response test cases
+    # TODO write nominal test cases
+    def test_add_user_response(self):
+        # db_methods.add_user_response()
+        pass
+
+    # TODO write edge cases for add_user_response
+
+    # get_form_metadata test cases
+    # TODO write nominal test cases
+    def test_get_form_metadata(self):
+        pass
+
+    # TODO write edge cases for get_form_metadata
