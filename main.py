0dd9e9ac16d5b67d58154e4d61c5cf6f9d0609b9
Brendan Chen <me@bchen.dev>
2024-09-27 15:45:44 -0700

    grab the database cursor when rendering index page

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
-# from markupsafe import escape
+from db import db
+from flask import Flask, g, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    cur = db.get_db().cursor()
+    print(cur)
+
     if request.method == 'POST':
         print("Form data submitted to server")
         return 'Form submitted'
     else:
         return render_template("sample.html")
+    
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
