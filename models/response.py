503976bd2d33faf557089d6ed891b1a0d7b4fce0
Brendan Chen <me@bchen.dev>
2024-09-28 16:16:50 -0700

    update spacing and add response id for response

--- models/response.py
+++ models/response.py
@@ -1,8 +1,16 @@
 from datetime import datetime
 
 class Response:
-    def __init__(self, selected_choice_id: int, question_id: int, associated_text = None, timestamp = datetime.now()):
+    def __init__(
+        self,
+        selected_choice_id: int,
+        question_id: int,
+        associated_text = None,
+        timestamp = datetime.now(),
+        response_id = None  # optional for responses not yet in database
+    ):
         self.selected_choice_id = selected_choice_id
         self.question_id = question_id
         self.timestamp = timestamp
         self.associated_text = associated_text
+        self.response_id = response_id
