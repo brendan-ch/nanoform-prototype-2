69ac3d6faca5b8d62688bd693da3dc908518d979
Brendan Chen <me@bchen.dev>
2024-09-28 16:14:39 -0700

    add Response model

--- models/response.py
+++ models/response.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 class Response:
-    def __init__(self):
-        self.timestamp = datetime.now()
-        
+    def __init__(self, selected_choice_id: int, question_id: int, associated_text = None, timestamp = datetime.now()):
+        self.selected_choice_id = selected_choice_id
+        self.question_id = question_id
+        self.timestamp = timestamp
+        self.associated_text = associated_text
