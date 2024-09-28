1f03659b149de527726ff4878bd0115d8dbf5af9
Brendan Chen <me@bchen.dev>
2024-09-28 16:39:32 -0700

    add more stub methods for repository

--- db/repository.py
+++ db/repository.py
@@ -1,6 +1,8 @@
 from flask import g
 from models.response import Response
 from models.form import Form
+from models.form_question import FormQuestion
+from models.question_choice import QuestionChoice
 import sqlite3
 
 DATABASE = './database.db'
@@ -8,6 +10,15 @@ DATABASE = './database.db'
 class Repository():
     def __init__(self, connection = sqlite3.connect(':memory:', check_same_thread=False)):
         self.connection = connection
+
+    def add_form(self, form: Form):
+        pass
+
+    def add_question(self, question: FormQuestion):
+        pass
+
+    def add_question_choice(self, question_choice: QuestionChoice):
+        pass
     
     def add_user_response(self, response: Response):
         pass
