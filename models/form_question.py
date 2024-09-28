da8cf4e3da11f5afbfb517a7cbff97a3d21c4cb2
Brendan Chen <me@bchen.dev>
2024-09-28 16:23:59 -0700

    add form question model

---
+++ models/form_question.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+
+class FormQuestionType(Enum):
+    SHORT_RESPONSE = 1
+    LONG_RESPONSE = 2
+    MULTIPLE_CHOICE = 3
+
+@dataclass
+class FormQuestion:
+    question_type: FormQuestionType
+    question_name: str
+    question_id: Optional[int] = None
+    form_id: Optional[int] = None
