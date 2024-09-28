2ac9a4105dad13f3b9a8d940ffe7afd9d90a64cd
Brendan Chen <me@bchen.dev>
2024-09-28 16:17:50 -0700

    switch to python dataclass annotation

--- models/response.py
+++ models/response.py
@@ -1,16 +1,11 @@
 from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
 
+@dataclass
 class Response:
-    def __init__(
-        self,
-        selected_choice_id: int,
-        question_id: int,
-        associated_text = None,
-        timestamp = datetime.now(),
-        response_id = None  # optional for responses not yet in database
-    ):
-        self.selected_choice_id = selected_choice_id
-        self.question_id = question_id
-        self.timestamp = timestamp
-        self.associated_text = associated_text
-        self.response_id = response_id
+    selected_choice_id: int
+    question_id: int
+    associated_text: Optional[str] = None
+    response_id: Optional[int] = None
+    timestamp: datetime = field(default_factory=datetime.now)
