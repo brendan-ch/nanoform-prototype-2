56a1047c731699725feacbe4f4b3ba60bc9a94e7
Brendan Chen <me@bchen.dev>
2024-09-29 16:02:32 -0700

    add question_id field to question_choice model

--- models/question_choice.py
+++ models/question_choice.py
@@ -5,5 +5,6 @@ from typing import Optional
 class QuestionChoice:
     choice_name: str
     choice_position: int
+    question_id: int
     choice_id: Optional[int] = None
     has_free_response_field: bool = False
