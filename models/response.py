6b36dc2ffe84f4768f2e5fb37420d8591ec7d4ca
Brendan Chen <me@bchen.dev>
2024-09-29 16:15:55 -0700

    add response_choice model

--- models/response.py
+++ models/response.py
@@ -5,7 +5,11 @@ from typing import Optional
 @dataclass
 class Response:
     question_id: int
-    selected_choice_ids: int | list[int] = field(default_factory=lambda: [])
-    associated_text: Optional[str] = None
     response_id: Optional[int] = None
     timestamp: datetime = field(default_factory=datetime.now)
+
+@dataclass
+class ResponseChoice:
+    choice_id: int
+    response_id: int
+    associated_text: Optional[str] = None
